53324a1ef94a2c5635d3aa1c8f9ed7e1ee957dbc
Callum <callumpetermason@googlemail.com>
2019-03-17 18:12:46 +0000

    Initial test code for testing motor

---
+++ Motor/Motor/main.cpp
@@ -0,0 +1,36 @@
+#include <wiringPi.h>
+
+void steps(int delay, int steps) {
+	for (int i = 0; i < steps; i++)
+	{
+		setstep(1, 0, 0, 0);
+		delay(delay);
+		setstep(0, 1, 0, 0);
+		delay(delay);
+		setstep(0, 0, 1, 0);
+		delay(delay);
+		setstep(0, 0, 0, 1);
+		delay(delay);
+	}
+}
+
+void setstep(int w1, int w2, int w3, int w4) {
+	digitalWrite(0, w1);
+	digitalWrite(1, w2);
+	digitalWrite(2, w3);
+	digitalWrite(3, w4);
+	
+}
+
+int main(void)
+{
+	wiringPiSetup();
+	pinMode(0, OUTPUT);
+	pinMode(1, OUTPUT);
+	pinMode(2, OUTPUT);
+	pinMode(3, OUTPUT);	
+	
+	steps(200, 10);
+	
+	return 0;
+}
