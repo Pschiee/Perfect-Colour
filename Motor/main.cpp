bd6afbf0ce5fda1e6dbeae3de6ca40fafc7fa83f
Callum <callumpetermason@googlemail.com>
2019-03-25 13:35:13 +0000

    Committing changes

--- Motor/main.cpp
+++ Motor/main.cpp
@@ -1,5 +1,21 @@
 #include <wiringPi.h>
 
+/*! \fn void setstep(int w1, int w2, int w3, int w4)
+	\brief Moves the motor 4 steps
+
+	\param w1 1 or 0 to determine if coil 1 is on or off
+	\param w2 1 or 0 to determine if coil 2 is on or off
+	\param w3 1 or 0 to determine if coil 3 is on or off
+	\param w4 1 or 0 to determine if coil 4 is on or off
+*/
+
+/*! \fn void steps(int time, int steps)
+	\brief Moves the motor the number of steps
+
+	\param time Delay between each step
+	\param steps Number of steps to move	
+*/
+
 void setstep(int w1, int w2, int w3, int w4) {
 	digitalWrite(0, w1);
 	digitalWrite(1, w2);
