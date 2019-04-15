14f9a1d8034951b879f1be39cb06882644bd3146
Pschiee <48247391+Pschiee@users.noreply.github.com>
2019-04-15 17:21:02 +0100

    Update HX711.cpp

--- Software/sources/HX711.cpp
+++ Software/sources/HX711.cpp
@@ -1,10 +1,8 @@
 /**
  * @file HX711.cpp
- * @author Callum Mason + Silviya Ivanova + Leire Froufe Vigara
  * @brief The class HX711 provides the functionality to obtain the weight on the load cell.
  * @date 2019-04-12
- *
- * @copyright Copyright (c) 2019
+   @author agu
  *
  */
 
